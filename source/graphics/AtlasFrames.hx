e90529da11ac3f7c99e8623ae1fa4ad7d3fea292
matthewfroncek861-svg <matthewfroncek861@gmail.com>
2025-12-03 03:09:46 -0500

    Update AtlasFrames.hx

--- source/graphics/AtlasFrames.hx
+++ source/graphics/AtlasFrames.hx
@@ -1,69 +1,67 @@
 package graphics;
 
+import haxe.Json;
 import haxe.io.Bytes;
 import flixel.FlxG;
+import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxAtlasFrames;
-import flixel.math.FlxMatrix;
-import flixel.math.FlxRect;
 import flixel.graphics.frames.FlxFrame;
-import flxanimate.data.SpriteMapData.AnimateAtlas;
-import flxanimate.data.SpriteMapData.AnimateSpriteData;
-import flxanimate.data.SpriteMapData.Meta;
-import flxanimate.frames.FlxAnimateFrames;
+import flixel.math.FlxRect;
 import openfl.Assets;
 import openfl.display.BitmapData;
 
 /**
- * Alternative for getting FlxAnimateFrames.
+ * 100% flxanimate-free atlas loader
+ * Supports: spritemap1.json, spritemap2.json, etc.
+ *
+ * Returns regular FlxAtlasFrames (Psych-compatible)
  */
-class AtlasFrames extends FlxAnimateFrames
-{
-	public static function textureAtlas(Path:String):FlxAtlasFrames
-	{
-		var frames:FlxAnimateFrames = new FlxAnimateFrames();
+class AtlasFrames {
+	/**
+	 * Loads all spritemap{i}.json + its PNG into a combined FlxAtlasFrames.
+	 */
+	public static function textureAtlas(path:String):FlxAtlasFrames {
+		var masterFrames:FlxAtlasFrames = null;
+		var i:Int = 1;
 
-		var i = 1;
-		while (Assets.exists('$Path/spritemap$i.json'))
-		{
-			var curJson:AnimateAtlas = haxe.Json.parse(StringTools.replace(Assets.getText('$Path/spritemap$i.json'), String.fromCharCode(0xFEFF), ""));
-			var curSpritemap = Assets.getBitmapData('$Path/${curJson.meta.image}');
+		while (Assets.exists('$path/spritemap$i.json')) {
+			var jsonText = Assets.getText('$path/spritemap$i.json');
+			if (jsonText == null)
+				break;
 
-			if (curSpritemap != null)
-			{
-				var spritemapFrames = FlxAnimateFrames.fromSpriteMap(curJson, curSpritemap);
-				if (spritemapFrames != null)
-					frames.addAtlas(spritemapFrames);
+			var parsed:Dynamic = Json.parse(jsonText);
+			var imageName:String = parsed.meta.image;
+
+			var bmp:BitmapData = Assets.getBitmapData('$path/$imageName');
+			if (bmp == null) {
+				FlxG.log.error('Missing PNG: $path/$imageName');
+				i++;
+				continue;
 			}
+
+			var graphic = FlxGraphic.fromBitmapData(bmp);
+			var frames = FlxAtlasFrames.findFrame(graphic);
+
+			if (masterFrames == null)
+				masterFrames = frames;
 			else
-				FlxG.log.error('the image called "${curJson.meta.image}" does not exist in Path $Path, maybe you changed the image Path somewhere else?');
+				masterFrames.frames = masterFrames.frames.concat(frames.frames);
+
 			i++;
 		}
-		if (frames.frames == [])
-		{
-			FlxG.log.error("the Frames parsing couldn't parse any of the frames, it's completely empty! \n Maybe you misspelled the Path?");
+
+		if (masterFrames == null) {
+			FlxG.log.error('[AtlasFrames] No spritemaps found in: $path');
 			return null;
 		}
-		return frames;
+
+		return masterFrames;
 	}
 
-	static function atlasHelper(SpriteMap:BitmapData, limb:AnimateSpriteData, curMeta:Meta)
-	{
-		var width = (limb.rotated) ? limb.h : limb.w;
-		var height = (limb.rotated) ? limb.w : limb.h;
-		var sprite = new BitmapData(width, height, true, 0);
-		var matrix = new FlxMatrix(1, 0, 0, 1, -limb.x, -limb.y);
-		if (limb.rotated)
-		{
-			matrix.rotateByNegative90();
-			matrix.translate(0, height);
-		}
-		sprite.draw(SpriteMap, matrix);
-
-		@:privateAccess
-		var curFrame = new FlxFrame(FlxG.bitmap.add(sprite));
-		curFrame.name = limb.name;
-		curFrame.sourceSize.set(width, height);
-		curFrame.frame = new FlxRect(0, 0, width, height);
-		return curFrame;
+	public static function getFrame(frames:FlxAtlasFrames, name:String):FlxFrame {
+		for (f in frames.frames)
+			if (f.name == name)
+				return f;
+		return null;
 	}
 }
