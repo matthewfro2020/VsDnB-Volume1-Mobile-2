0a0cdb8aabb66bae7fcd494319224225ad8bb694
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-04 07:15:27 -0500

    Create ScriptedClass.hx

---
+++ source/polymod/hscript/ScriptedClass.hx
@@ -0,0 +1,11 @@
+package polymod.hscript;
+
+/**
+ * Alias for HScriptedClass used by some engine forks.
+ * Polymod normally provides this, but your version does not.
+ */
+class ScriptedClass extends HScriptedClass {
+    public function new() {
+        super();
+    }
+}
