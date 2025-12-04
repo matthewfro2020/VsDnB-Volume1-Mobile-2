08650cd876b359361d55b28efb886f7dbcef8dae
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-12-04 07:15:47 -0500

    Create HScriptedClassMacro.hx

---
+++ source/polymod/hscript/_internal/HScriptedClassMacro.hx
@@ -0,0 +1,11 @@
+package polymod.hscript._internal;
+
+/**
+ * Stub macro so engines that compile macro hooks do not fail.
+ * No macro logic is executed.
+ */
+class HScriptedClassMacro {
+    macro public static function build() {
+        return macro {};
+    }
+}
